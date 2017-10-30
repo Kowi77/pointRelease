1d18fb4715bebfc676ae0c2bc1e064fca43ef25a
Kowi77 <kovalenka@mail.ru>
2017-10-31 06:57:14 +0700

    ver 1.2

---
+++ src/main/java/kov/develop/server/HibernateUtil.java
@@ -0,0 +1,34 @@
+package kov.develop.server;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+
+public class HibernateUtil {
+
+    private static SessionFactory sessionFactory = buildSessionFactory();
+
+    protected static SessionFactory buildSessionFactory() {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure()
+                .build();
+        try {
+            sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
+        }
+        catch (Exception e) {
+            StandardServiceRegistryBuilder.destroy( registry );
+        }
+        return sessionFactory;
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+
+        getSessionFactory().close();
+    }
+}
