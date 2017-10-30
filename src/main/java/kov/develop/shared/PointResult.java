f776dd97f2c692b340a98a6baaef0c534752021a
Kowi77 <kovalenka@mail.ru>
2017-10-30 23:41:37 +0700

    ver 1.1

--- src/main/java/kov/develop/shared/PointResult.java
+++ src/main/java/kov/develop/shared/PointResult.java
@@ -1,46 +1,27 @@
 package kov.develop.shared;
 
-import javax.persistence.*;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlType;
+import kov.develop.shared.Point;
+import kov.develop.shared.PointType;
+
 import java.io.Serializable;
 
 /**
- * Entity and Xml class
+ * DTO class
  */
-@Entity
-@Table(name="points")
-@XmlRootElement(name="point")
-@XmlType(propOrder = {"id", "country", "sity", "adress", "name", "phone", "type"})
-public class Point implements Serializable {
+public class PointResult implements Serializable{
 
-    @Id
-    @Column(name="id", nullable = false)
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
-    @Basic
-    @Column(name="country", nullable = false, length = 30)
     private String country;
-    @Basic
-    @Column(name="sity", nullable = false, length = 30)
     private String sity;
-    @Basic
-    @Column(name="adress", nullable = false, length = 30)
     private String adress;
-    @Basic
-    @Column(name="name", nullable = false, length = 50)
     private String name;
-    @Basic
-    @Column(name="phone", nullable = false, length = 18)
     private String phone;
-    @Basic
-    @Column(name="type", nullable = false, length = 20)
     private PointType type;
 
-    public Point() {
+    public PointResult() {
     }
 
-    public Point(String country, String sity, String adress, String name, String phone, PointType type) {
+    public PointResult(String country, String sity, String adress, String name, String phone, PointType type) {
         this.country = country;
         this.sity = sity;
         this.adress = adress;
@@ -49,7 +30,7 @@ public class Point implements Serializable {
         this.type = type;
     }
 
-    public Point(int id, String country, String sity, String adress, String name, String phone, PointType type) {
+    public PointResult(int id, String country, String sity, String adress, String name, String phone, PointType type) {
         this.id = id;
         this.country = country;
         this.sity = sity;
@@ -57,6 +38,16 @@ public class Point implements Serializable {
         this.name = name;
         this.phone = phone;
         this.type = type;
+    }
+
+    public PointResult(Point point){
+        this.id = point.getId();
+        this.country = point.getCountry();
+        this.sity = point.getSity();
+        this.adress = point.getAdress();
+        this.name = point.getName();
+        this.phone = point.getPhone();
+        this.type = point.getType();
     }
 
     public int getId() {
---
+++ src/main/java/kov/develop/shared/PointType.java
@@ -0,0 +1,9 @@
+package kov.develop.shared;
+
+public enum PointType {
+
+    ОТПРАВКА,
+    ВЫДАЧА,
+    ОТПРАВКА_И_ВЫДАЧА;
+
+}
